c448fd0b3a034eed9a3826cdc906db3bab36ee54
jzm2103 Mei <jzm2103@columbia.edu>
2023-11-01 03:25:03 +0000

    rspec tests (everything besides controller)

--- rottenpotatoes/app/models/goods_and_service.rb
+++ rottenpotatoes/app/models/goods_and_service.rb
@@ -1,14 +1,6 @@
 class GoodsAndService < ActiveRecord::Base
   belongs_to :user, class_name: 'UserInformation', foreign_key: 'user_id', primary_key: 'user_id'
 
-  def others_by_same_connection
-    GoodsAndService.where(connection: self.connection).where.not(id: self.id)
-  end
-  
-  def self.with_connection(connection)
-    where(connection: connection)
-  end
-
   def self.all_categories
     ['Good', 'Service']  
   end
---
+++ rottenpotatoes/spec/models/goods_and_service_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe UserInformation, type: :model do
+  # Let's say you have the following setup:
+  let!(:person1) { UserInformation.create!(user_id: "1234", username: "person 1", name: "A", friends: []) }
+  let!(:person2) { UserInformation.create!(user_id: "2345", username: "person 2", name: "B", friends: ['1234']) }
+  let!(:person3) { UserInformation.create!(user_id: "3456", username: "person 3", name: "C", friends: ['1234', '2345']) }
+
+
+  describe '#validate_friends_array' do
+    it "returns early when 'friends' is empty" do
+
+        allow(person2).to receive(:friends).and_return([])
+        result = person2.validate_friends_array
+        expect(result).to be_nil
+    end
+
+    it "performs some action when 'friends' is not empty" do
+        # Stub the 'friends' method to return a non-empty array
+        allow(person3).to receive(:friends).and_return(['1234', '2345'])
+    end
+  end
+end 
+
+RSpec.describe GoodsAndService, type: :model do
+    let!(:item1)   { GoodsAndService.create!(user_id: "1234", name: "item1", category: 0, description: "A")}
+    let!(:item2)   { GoodsAndService.create!(user_id: "1234", name: "item2", category: 1, description: "B")}
+
+    
+    describe ".all_categories" do
+        it "returns an array of category names" do
+          categories = GoodsAndService.all_categories
+          expect(categories).to match_array(['Good', 'Service'])
+        end
+    end
+
+    describe ".with_categories" do
+        it "returns records with the specified categories" do
+            # Perform a query with the 'with_categories' method
+            result = GoodsAndService.with_categories(['Good'])
+    
+            # Expectations
+            expect(result).to include(item1)
+            expect(result).not_to include(item2)
+        end
+    end
+end
