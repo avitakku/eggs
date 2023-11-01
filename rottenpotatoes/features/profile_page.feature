9d698e8c903a7cbb1ea752b2131796ddb856855a
ag4049 AvantikaGarg <ag4049@columbia.edu>
2023-11-01 01:42:13 +0000

    done w cucumber tests!

--- rottenpotatoes/features/profile_page.feature
+++ rottenpotatoes/features/profile_page.feature
@@ -9,9 +9,21 @@ Feature: Profile Page Interaction
     Given I am on the Eggs home page
     When I click on the Edit Profile link
     Then I confirm that I am on the "Aryan Ray" profile page
-    Then I should see the username "ar4179"
-    Then I should see the address "70 Morningside Dr."
-    Then I should see the contact "ar4179@columbia.edu"
+    And I should see the username "ar4179"
+    And I should see the address "70 Morningside Dr."
+    And I should see the contact "ar4179@columbia.edu"
     And I should see a rating of "4.7"
-    And I should see the goods/services provided by "Aryan Ray"
-    And I should see a good named "Mini Fridge" in the category "Good"
+    And the following items should exist:
+    | name          | category |
+    | Mini Fridge   | Good     |
+    
+
+  Scenario: User wants to offer a new good/service
+    Given I am on the Eggs home page
+    When I click on the Edit Profile link
+    Then I confirm that I am on the "Aryan Ray" profile page
+    When I click on the Offer a new good/service link
+    Then I confirm that I am on the Offer a New Good or Service page
+    And I should see a text box for name
+    And I should see a text box for item description
+    And I should see a Post button
--- rottenpotatoes/features/step_definitions/items_steps.rb
+++ rottenpotatoes/features/step_definitions/items_steps.rb
@@ -66,29 +66,78 @@ end
 
 When /^I fill in 'Search-bar' with "(.*?)"$/ do |keyword|
     fill_in('Search-bar', with: keyword)
-  end
+end
 
 When /I click on the Edit Profile link/ do
     click_link("Edit Profile")
 end
 
 Then /^I confirm that I am on the "(.*?)" profile page$/ do |name|
-  puts page.body
+  #puts page.body
   expect(current_path).to eq(profile_path)
 end
 
-Then /I should see the username "([^"]*)"/ do |username|
+Then /I should see the username "(.*)"/ do |username|
   expect(page).to have_content(username)
 end
 
-Then /I should see the address "([^"]*)"/ do |address|
+Then /I should see the address "(.*)"/ do |address|
   expect(page).to have_content(address)
 end
 
-Then /I should see the contact "([^"]*)"/ do |contact|
+Then /I should see the contact "(.*)"/ do |contact|
   expect(page).to have_content(contact)
 end
 
-Then /I should see a rating of "([^"]*)"/ do |rating|
+Then /I should see a rating of "(.*)"/ do |rating|
   expect(page).to have_content(rating)
 end
+
+And /^the following items should exist:$/ do |table|
+  table.hashes.each do |item|
+    category = (item['category'] == 'Good' ? 0 : 1) 
+    GoodsAndService.create!(name: item['name'], category: category) 
+  end
+end
+
+When(/^I click on the Offer a new good\/service link$/) do
+  click_link('Offer a new good/service') 
+end
+
+Then(/^I confirm that I am on the Offer a New Good or Service page$/) do
+  expect(current_path).to eq(new_goods_service_path) 
+end
+
+Then("I should see a text box for name") do
+  expect(page).to have_css('input#goods_service_name')
+end
+
+Then("I should see a text box for item description") do
+  expect(page).to have_css('textarea#goods_service_description')
+end
+
+Then("I should see a Post button") do
+  expect(page).to have_css('input[type="submit"][name="commit"]')
+end
+
+
+Given(/^I visit the "Offer a New Good or Service" page$/) do
+  visit new_goods_service_path 
+end
+
+When(/^I fill in value for "(.*?)" with "(.*?)"$/) do |field, value|
+  fill_in(field, with: value)
+end
+
+When(/^I select category "(.*?)" from "(.*?)"$/) do |category, field|
+  select(category, from: field)
+end
+
+When(/^I click the "Post" button$/) do
+  click_button('Post') 
+end
+
+Then(/^I should see the new item "(.*?)" on my profile$/) do |item_name|
+  expect(page).to have_content(item.name)
+end
+
